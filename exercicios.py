5401bd6ddc659463bc7a43ae474dcbd7632aec6f
Vinícius Oliveira <65806032+Oliver00x@users.noreply.github.com>
2021-03-02 01:52:30 -0300

    Atualização 02/03/21 - exercicios.py

--- exercicios.py
+++ exercicios.py
@@ -1,4 +1,37 @@
 
 
-def calcula_media(notas):
-    return 0.0  # corrigir
+
+def calcula_media(boletim):
+    if len(boletim) == 0:
+        boletim.append(0.0)
+        quantidadenotas = len(boletim)
+        soma = sum(boletim)
+        media = soma / quantidadenotas
+        print(f"Sua média final foi {media:.2f}")
+    else:
+        quantidadenotas = len(boletim)
+        soma = sum(boletim)
+        media = soma / quantidadenotas
+        print(f"Sua média final foi {media:.2f}")
+
+
+def mensagem_saudacao(periodo):
+    if periodo == "M":
+        periodo = "Bom dia"
+    elif periodo == "V":
+        periodo = "Boa tarde"
+    elif periodo == "N":
+        periodo = "Boa noite"
+    else:
+        periodo = "Olá"
+    return print(periodo)
+
+
+def calcula_situacao(nota):
+    if nota == 10:
+        situacao = "Aprovado com Distinção!"
+    elif nota >= 7:
+        situacao = "Aprovado!"
+    elif nota < 7:
+        situacao = "Reprovado!"
+    return print(situacao)
